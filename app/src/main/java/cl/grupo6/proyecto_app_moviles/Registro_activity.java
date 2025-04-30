406224597c5b0e94f7128b2863b6dd3fc832ab21
FranciscoAlumnoUbb <francisco.aguilar1801@alumnos.ubiobio.cl>
2025-04-29 23:19:27 -0400

    nuevas vistas

---
+++ app/src/main/java/cl/grupo6/proyecto_app_moviles/Registro_activity.java
@@ -0,0 +1,20 @@
+package cl.grupo6.proyecto_app_moviles;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Registro_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_registro);
+
+    }
+}
