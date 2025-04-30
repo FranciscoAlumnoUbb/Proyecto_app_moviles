1a6622d3ab5f15a2af4cdec7ae4614a652a5a3c1
FranciscoAlumnoUbb <francisco.aguilar1801@alumnos.ubiobio.cl>
2025-04-30 00:51:50 -0400

    Prueba de logica para cerrar sesion

--- app/src/main/java/cl/grupo6/proyecto_app_moviles/Menu_principal_activity.java
+++ app/src/main/java/cl/grupo6/proyecto_app_moviles/Menu_principal_activity.java
@@ -1,6 +1,9 @@
 package cl.grupo6.proyecto_app_moviles;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +18,16 @@ public class Menu_principal_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
 
+        Button cerrarSesion = (Button) findViewById(R.id.btncerrarsesion);
+
+        cerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Menu_principal_activity.this, MainActivity.class));
+                finish();
+            }
+        });
+
     }
+
 }
